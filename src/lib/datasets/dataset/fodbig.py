6cb6687ddcb5922b0f8c463b8f8f9a6cda8b47cb
moonwhy <1162717648@qq.com>
2019-12-02 23:01:47 +0800

    fodeval

--- src/lib/datasets/dataset/fodbig.py
+++ src/lib/datasets/dataset/fodbig.py
@@ -3,7 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 import pycocotools.coco as coco
-from pycocotools.cocoeval import COCOeval
+from pycocotools.fodeval import FODeval
 import numpy as np
 import json
 import os
@@ -109,7 +109,7 @@ class FOD(data.Dataset):
         # json.dump(detections, open(result_json, "w"))
         self.save_results(results, save_dir)
         coco_dets = self.coco.loadRes('{}/results.json'.format(save_dir))
-        coco_eval = COCOeval(self.coco, coco_dets, "bbox")
+        coco_eval = FODeval(self.coco, coco_dets, "bbox", self.class_name[1:])
         coco_eval.evaluate()
         coco_eval.accumulate()
         coco_eval.summarize()
